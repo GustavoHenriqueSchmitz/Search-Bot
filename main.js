5a0809717cc06fbbeaba37b14cc705047e7a9496
GustavoHenriqueSchmitz <gustavohenriqueschmitz568@gmail.com>
2022-10-12 23:08:10 -0300

    opening and seraching

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-'use strict';
+"use strict";
 exports.__esModule = true;
-const selenium_webdriver_1 = require('selenium-webdriver');
-function example() {
-  const driver = new selenium_webdriver_1.Builder()
-    .forBrowser('firefox')
-    .build();
-  driver.get('https://www.google.com');
-}
-example();
+var selenium_webdriver_1 = require("selenium-webdriver");
+var driver = new selenium_webdriver_1.Builder().forBrowser('firefox').build();
+driver.get('https://www.google.com');
+driver
+    .findElement(selenium_webdriver_1.By.xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input'))
+    .sendKeys('Notícias', selenium_webdriver_1.Key.ENTER);
--- main.ts
+++ main.ts
@@ -1,4 +1,11 @@
-import {Builder, WebDriver} from 'selenium-webdriver';
+import {Builder, By, Key, WebDriver} from 'selenium-webdriver';
 
 const driver: WebDriver = new Builder().forBrowser('firefox').build();
 driver.get('https://www.google.com');
+driver
+  .findElement(
+    By.xpath(
+      '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input'
+    )
+  )
+  .sendKeys('Notícias', Key.ENTER);
